adc8e594bea378c85a2e2d5d0171b7c2f8989ddc
FagundesMatheus <fagundes2@discente.ufg.br>
2024-06-18 19:15:01 -0300

    atualização

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Post("/", handlers.Create) //funcionando
-	r.Put("/{id}", handlers.Update) 
+	r.Put("/{id}", handlers.Update)  //funcionando
 	r.Delete("/{id}", handlers.Delete) //funcionando
 	r.Get("/{id}", handlers.Get) //funcionando
 	r.Get("/", handlers.List) //funcionando
