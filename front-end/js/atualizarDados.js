9d21bdfa9d8c3be1451215e63da6b64a446532d9
FagundesMatheus <fagundes2@discente.ufg.br>
2024-07-11 20:39:45 -0300

    atualização de dados

--- front-end/js/atualizarDados.js
+++ front-end/js/atualizarDados.js
@@ -1,10 +1,4 @@
 let pacienteData = {};
-let id = 0
-let endereco = ""
-let bairro = ""
-let telefone = ""
-let atividade = true
-let email = ""
 
 document.getElementById("searchForm").addEventListener("submit", function(event) {
     event.preventDefault();
@@ -40,17 +34,24 @@ document.getElementById("searchForm").addEventListener("submit", function(event)
                     break;
             }
             let h2Element = document.getElementById('nome');
-            endereco = pacienteData.endereco
-            bairro = pacienteData.bairro
-            telefone = pacienteData.telefone
-            email = pacienteData.email
-            atividade = pacienteData.atividade
+            let h2Element2 = document.getElementById('nome2');
+            let h2Element3 = document.getElementById('nome3')
+
+
             
             const paciente = data[0]; 
             pacienteData = { ...paciente }; // Copia os dados do paciente para o objeto pacienteData
 
+            localStorage.setItem('id', pacienteData.id);
+            localStorage.setItem('endereco', pacienteData.endereco);
+            localStorage.setItem('bairro', pacienteData.bairro);
+            localStorage.setItem('telefone', pacienteData.telefone);
+            localStorage.setItem('email', pacienteData.email);
+            localStorage.setItem('atividade', pacienteData.atividade);
 
             h2Element.textContent = pacienteData.nome_completo;
+            h2Element2.textContent = pacienteData.nome_completo;
+            h2Element3.innerHTML = 'Deseja inativar<br>' + pacienteData.nome_completo + '?';
 
 
 
@@ -66,3 +67,55 @@ document.getElementById("searchForm").addEventListener("submit", function(event)
         alert("Erro ao buscar paciente.");
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var sim = document.getElementById('sim');
+    sim.addEventListener('click', function() {
+            atividade = false
+            atualizarDado()
+            var elemento = document.getElementById('popup3');
+            elemento.removeAttribute('hidden');
+
+            var elemento2 = document.getElementById('popupInativar');
+            elemento2.setAttribute('hidden', true);
+        });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var nao = document.getElementById('nao');
+    nao.addEventListener('click', function() {
+            localStorage.setItem('atividade', 'false');
+            var elemento2 = document.getElementById('popupInativar');
+            elemento2.setAttribute('hidden', true);
+        });
+});
+
+
+function atualizarDado() {
+
+    let formData = {
+        Endereco: localStorage.getItem('endereco'),
+        Bairro: localStorage.getItem('bairro'),
+        Email: localStorage.getItem('email'),
+        Telefone: localStorage.getItem('telefone'),
+        Atividade: localStorage.getItem('atividade') === 'true'
+    };
+    let requestOptions = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    };
+    console.log(JSON.stringify(formData))
+    let url = 'http://localhost:9000/{id}';
+
+    /*fetch(url, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            console.log('Resposta da API:', data);
+
+        })
+        .catch(error => console.error('Erro ao enviar dados:', error));*/
+}
+
+
